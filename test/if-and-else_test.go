0eba8a3127b42c4e18b2e037cea47a0d5835b525
Viktor Levchenko <levchenko.victor@example.com>
2020-04-25 14:04:03 +0000

    comples if test example

---
+++ test/if-and-else_test.go
@@ -0,0 +1,24 @@
+package test
+
+import (
+	"fmt"
+	"math"
+	"testing"
+)
+
+func pow2(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	} else {
+		fmt.Printf("%g >= %g\n", v, lim)
+	}
+	// can't use v here, though
+	return lim
+}
+
+func TestPow2(t *testing.T) {
+	fmt.Println(
+		pow(3, 2, 10),
+		pow(3, 3, 20),
+	)
+}
